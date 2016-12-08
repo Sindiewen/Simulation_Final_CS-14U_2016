e8f0a103e1fdbb1a7b6f520b5f0f7f6ca757e61b
Rachel Vancleave <sindiewen@gmail.com>
2016-12-07 16:49:49 -0800

    Added GUI Text showing each actors current BPM and Max BPM

---
+++ Simulation Final/Assets/_Scripts/_Scripts/GUIActorBPMDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIActorBPMDisplay : MonoBehaviour 
+{
+	// Public Variables
+	public Text GUIBPMCurrentText;		// Stores a reference to the UI Text GameObject for Current BPM Text
+	public Text GUIBPMMaxText;			// Stores a reference to the UI Text GameObject for Max BPM Text
+
+	// private variables
+	private TravelerController Actor;	// Object of the Traveler Controlle Class
+
+	void Start () 
+	{
+		// Gets this GameObject's Traveler Controller componenet
+		Actor = this.GetComponent<TravelerController>();
+	}
+	
+	void Update () 
+	{
+		// Changes the GUI BPM Count text to associate the current BPM
+		GUIBPMCurrentText.text = Actor.currentBPM.ToString();
+
+		// Changes the MaxBPM Text to show the max BPM of the actor
+		GUIBPMMaxText.text = Actor.maxBPM.ToString();
+
+	}
+}
